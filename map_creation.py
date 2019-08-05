68f5b1dfd4c6c4ad3703a560220a270c5d5f9b69
David Tenorio <dtenorio.work@gmail.com>
2019-08-05 15:46:31 -0600

    updated map to show different colors based on arbitrary data, added geoJSON file and dependency to the main Python file

--- map_creation.py
+++ map_creation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug  3 11:10:09 2019
+Created on Sat Aug 3 11:10:09 2019
 
 Python 3.x
 @authors: David Tenorio
@@ -16,21 +16,24 @@ import random
 from geojson import Polygon, Feature, FeatureCollection, dump # 
 
 def importData(fname):    
-    
+    """imports a tsv, returns a pandas DataFrame"""
     df = pd.read_csv(fname, sep='\t', header=0) #tab separated file    
     return df    
 
-def squareMaker(long,lat):
-    size = 1
+def squareMaker(lat,long):
+    """helper function to return a list of coordinates representing a square centered on 
+        the two coordinates provided as inputs"""
+    SIZE = .002 #make the square bigger or smaller by changing this
     return[
-           (long+size,lat+size),
-           (long-size,lat+size),
-           (long-size,lat-size),
-           (long+size,lat-size),
-           (long+size,lat+size)
+           (lat+SIZE,long+SIZE),
+           (lat+SIZE,long-SIZE),
+           (lat-SIZE,long-SIZE),
+           (lat-SIZE,long+SIZE),
+           (lat+SIZE,long+SIZE)
            ]
 
 def makeMap(df):
+    """takes in a pandas DataFrame, uses folium to produce a map of the different schools"""
     nrows, ncols = df.shape
     
     init_coord = [35.111135, -106.633373]
@@ -40,47 +43,52 @@ def makeMap(df):
     zoom_start=11
     )
     
+    #for now, make some dummy data to show that we can plot data when we have it
     dummy = []
     for x in range(35):
         dummy.append(random.randint(1,100))
     
-    #df["dummy_data"] = dummy
-    df["dummy_data"] = range(35)
+    df["dummy_data"] = dummy
+    #df["dummy_data"] = range(35)
     
     features = [] #for geoJSON data
     
+    #go through each school
     for r in range(nrows):
+        #get the coordinates and name
         long = round(df.loc[r]["Long"],6)
         lat = round(df.loc[r]["Lat"],6)
         name =  df.loc[r]["School Name"]
         
-        
-        coords = squareMaker(long,lat)
+        #use geoJSON to create a square centered around each school
+        coords = squareMaker(lat,long) #for geoJSON, use the order lat, long
         poly = Polygon([coords])
-        print(poly)
+        
+        
         features.append(Feature(geometry=poly, id= name))
         
-        
+        #make a marker for each school using folium
         folium.Marker(
-        location= [long,lat],
+        location= [long,lat], #for folium, use the order long,lat
         popup= name,
         icon=folium.Icon(color="green",prefix="fa",icon='graduation-cap')
         ).add_to(m)
         
-    
+    #save the geojson file locally
     feature_collection = FeatureCollection(features)
     with open('myfile.geojson', 'w') as f:
         dump(feature_collection, f)
     f.close()
     
-    #creating the heat map
+    #creating the choropleth map, which is a map based on a data value
     folium.Choropleth(
     #geo_data= 'other.json',
     geo_data= feature_collection,
     name='choropleth',
-    
+    data=df,
+    columns=["School Name", "dummy_data"],
     key_on='feature.id',
-    fill_color='BuPu',
+    fill_color='BuPu', #change the colors here - uses color brewer (http://colorbrewer2.org/) sequential palettes
     fill_opacity=0.7,
     line_opacity=0.9,
     legend_name='Dummy Data',
@@ -90,7 +98,8 @@ def makeMap(df):
     #folium.TileLayer(tiles='Stamen Terrain',name="Terrain").add_to(m)
     folium.LayerControl().add_to(m)
     
-    m.save('index2.html')
+    #save the map to an html file
+    m.save('index.html')
     
     
 if __name__ == "__main__":
